0ab234059ad6c552a9b61b4bc8f1638d6331008a
lihua <1642023704@qq.com>
2025-07-11 20:48:05 +0800

    add one java file

---
+++ Plus.java
@@ -0,0 +1,15 @@
+/**
+ * @author lihua
+ * @version 1.0
+ */
+public class Plus {
+    public static void main(String[] args){
+        System.out.println(100 + 99); //199
+        System.out.println("100" + 99);//10099
+        System.out.println(100 + 99 + "hello");//199hello
+        System.out.println("hello" + 100 + 99);//hello10099
+
+        byte n1=127;  //byte类型1个字节
+        System.out.println(n1);
+    }
+}
